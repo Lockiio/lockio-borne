7892037fd4fcaa567d7bac220f26c8269e9b7aa4
rsaurel <remi.saurel@kosmos.fr>
2023-02-07 11:11:58 +0100

    add: buttons and lockio logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import BigButton from "./components/BigButton.vue";
 </script>
 
 <template>
-  <div class="text-3xl font-bold underline">
-    Hello world
+  <div class="text-4xl font-bold mt-6">
+    Avez-vous un compte Lockio ?
+  </div>
+  <div class="flex flex-center justify-evenly mt-16">
+    <BigButton text="Oui, je me connecte" />
+    <BigButton text="Non, je continue en tant qu'invité" />
   </div>
 </template>
 
---
+++ src/components/BigButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="bg-gray-800 text-white font-bold py-10 px-6 rounded-xl text-5xl">
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  }
+})
+
+</script>
+
+<style scoped>
+
+button {
+  width: 360px;
+  height: 460px;
+}
+</style>
