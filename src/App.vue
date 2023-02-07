a65df561e8523ca691af91100dec7ed6026c99d1
rsaurel <remi.saurel@kosmos.fr>
2023-02-07 11:44:36 +0100

    add: try to call api

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
-<script setup>
-import BigButton from "./components/BigButton.vue";
-</script>
-
 <template>
   <div class="text-4xl font-bold mt-6">
     Avez-vous un compte Lockio ?
   </div>
   <div class="flex flex-center justify-evenly mt-16">
-    <BigButton text="Oui, je me connecte" />
-    <BigButton text="Non, je continue en tant qu'invité" />
+    <BigButton text="Oui, je me connecte" @click="switchLeds('on')"/>
+    <BigButton text="Non, je continue en tant qu'invité" @click="switchLeds('off')"/>
   </div>
 </template>
+
+<script setup>
+import BigButton from "./components/BigButton.vue";
+import axios from "axios";
+import { API_RASP_URL} from "./utils/constant.ts";
+
+const switchLeds = (type) => {
+  console.log("switchLeds", type);
+  axios.get(API_RASP_URL + "/led/" + type);
+}
+
+</script>
 
 <style scoped>
 
