b3a4810ad70f7cec57b509f703d93160dde3f582
RémiSaurel <r.saurel5@gmail.com>
2023-02-26 04:23:08 +0100

    fix: css and emits

--- src/components/CodeInput.vue
+++ src/components/CodeInput.vue
@@ -4,10 +4,12 @@
       type="password"
       required
       maxlength="4"
-      class="text-4xl text-center w-48 h-24 border-2 border-gray-800 rounded-2xl"
+      class="text-4xl text-center w-48 h-24 border-2 border-gray-800 rounded-2xl bg-white"
       @input="$emit('updateValue', $event.target.value)"
     />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineEmits(["updateValue"]);
+</script>
