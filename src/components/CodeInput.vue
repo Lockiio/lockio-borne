081132d87632b655f58b21f277ffb6e4ef4127a8
RémiSaurel <r.saurel5@gmail.com>
2023-03-06 22:27:16 +0100

    add: UI on code error

--- src/components/CodeInput.vue
+++ src/components/CodeInput.vue
@@ -2,17 +2,27 @@
   <div class="flex flex-center justify-evenly">
     <form>
       <input
-          type="password" pattern="\d{4,4}"
-          inputmode="numeric"
-          required
-          maxlength="4"
-          class="text-4xl text-center w-48 h-24 border-2 border-gray-800 rounded-2xl bg-white"
-          @input="$emit('updateValue', $event.target.value)"
+        type="password"
+        pattern="\d{4,4}"
+        inputmode="numeric"
+        required
+        maxlength="4"
+        :class="
+          props.codeIncorrect
+            ? 'border-red-500 text-red-500'
+            : 'border-gray-800'
+        "
+        class="text-4xl text-center w-48 h-24 border-2 rounded-2xl bg-white"
+        @input="$emit('updateValue', $event.target.value)"
       />
     </form>
   </div>
 </template>
 
 <script setup lang="ts">
+
 defineEmits(["updateValue"]);
+const props = defineProps<{
+  codeIncorrect: boolean;
+}>();
 </script>
